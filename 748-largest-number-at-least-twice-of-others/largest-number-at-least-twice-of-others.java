70216a7b85f1f2e6e5f7b8ec33d5d806c0bd8c04
Srikar Udathaneni <srikar18406@gmail.com>
2026-05-15 10:19:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (33.93%) - LeetSync

--- 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
@@ -1,13 +1,17 @@
 class Solution {
     public int dominantIndex(int[] nums) {
-        int []dummy = nums.clone();
-        Arrays.sort(dummy);
-        int x=dummy[dummy.length-1] - (dummy[dummy.length - 2] *2);
-        if(x>=0){
-            for(int i = 0 ; i<nums.length ; i++){
-                if(nums[i]==dummy[dummy.length - 1]) return i;
+        int l = nums[0] , sl = nums[0] , li = 0;
+        for(int i = 0 ; i<nums.length ; i++){
+            if(nums[i]>l){
+                l = nums[i];
+                li = i;
             }
         }
-        return -1;
+        for(int i = 0 ; i<nums.length ; i++){
+            if(i!=li && nums[i]*2 > l){
+                return -1;
+            }
+        }
+        return li;
     }
 }
