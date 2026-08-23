de59b1ab10efd404c5a31d80ced8f2ad721d725a
Srikar Udathaneni <srikar18406@gmail.com>
2026-08-23 09:50:37 +0530

    Time: 4 ms (74.65%) | Memory: 45.2 MB (68.09%) - LeetSync

--- 4381-check-ascii-palindromic/check-ascii-palindromic.java
+++ 4381-check-ascii-palindromic/check-ascii-palindromic.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean isPalindromic(String s) {
         StringBuilder res = new StringBuilder();
-        for(int i = 0 ; i<s.length() ; i++){
-            char ch = s.charAt(i);
-            int c = (int)ch;
-            res.append(String.format("%8s", Integer.toBinaryString(c)).replace(' ', '0'));
+        for(char c: s.toCharArray()){
+            String bit = Integer.toBinaryString(c);
+            while(bit.length()<8){
+                bit = "0" + bit;
+            }
+            res.append(bit);
         }
         int a = 0 , b = res.length()-1;
         while(a<b){
